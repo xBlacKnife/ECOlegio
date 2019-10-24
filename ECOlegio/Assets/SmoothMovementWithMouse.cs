250e5dee27acfbd12f9fee0e9300434dffc61384
Alejandro Villar Rubio <avr-98@hotmail.es>
2019-10-24 23:37:22 +0200

    La camara se mueve poniendo el raton en los bordes

---
+++ ECOlegio/Assets/SmoothMovementWithMouse.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothMovementWithMouse : MonoBehaviour {
+
+    Camera camera_;
+    float cameraDistance_;
+
+    public float limX_ = 50.0f;
+    public float camVelX_ = 50.0f;
+    // Use this for initialization
+    void Start () {
+        camera_ = Camera.main;
+        cameraDistance_ = camera_.nearClipPlane; // Distancia de la cámara al plano cercano
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 mousePos = Input.mousePosition;
+
+        Vector3 topLeft = Camera.main.ViewportToScreenPoint(new Vector3(0, 1, cameraDistance_));
+        Vector3 topRight = Camera.main.ViewportToScreenPoint(new Vector3(1, 1, cameraDistance_));
+
+        Vector3 pos = camera_.transform.position;
+        if (mousePos.x > -10.0f && mousePos.x < topLeft.x + limX_)
+            pos.x += -camVelX_;
+        else if (mousePos.x > topRight.x - limX_ && mousePos.x < topRight.x + 10.0f)
+            pos.x += camVelX_;
+
+        camera_.transform.position = Vector3.Lerp(camera_.transform.position, pos, camVelX_ * Time.deltaTime);
+    }
+}
--- ECOlegio/Assets/uAdventure/__Scripts/Runner/Appearance/HideInventory.cs
+++ ECOlegio/Assets/uAdventure/__Scripts/Runner/Appearance/HideInventory.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using uAdventure.Runner;
-
 public class HideInventory : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
         InventoryManager.Instance.Show = false;
-    }
+	}
+	
 }
