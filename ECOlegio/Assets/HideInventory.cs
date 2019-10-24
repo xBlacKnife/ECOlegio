d728b12872de6849ff7a3580d41c9c5c48010657
Nuria <nubango@ucm.es>
2019-10-24 15:40:12 +0200

    Se oculta el inventario (by Alejandro)

---
+++ ECOlegio/Assets/HideInventory.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using uAdventure.Runner;
+
+public class HideInventory : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        InventoryManager.Instance.Show = false;
+    }
+}
